e04f4989bf91ccee9f29acc521814dec7ad72c48
Aditya-k24 <80339924+Aditya-k24@users.noreply.github.com>
2023-02-15 01:38:48 +0530

    vitals

--- frontend/src/components/RecordVitals.jsx
+++ frontend/src/components/RecordVitals.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import {TbReportAnalytics} from 'react-icons/Tb'
+import {RiVirusLine} from 'react-icons/Ri'
+import {BiDonateBlood} from 'react-icons/Bi'
+import {GiMedicalPack} from 'react-icons/Gi'
 
 export default function RecordVitals() {
   return (
-    <div>
-        <h4 className='position-absolute top-50 start-0 translate-middle-y'>Record Vitals</h4>
-        <div class="overflow-x-scroll ...">
-
+    <div className='main_vital_container'>
+        <h4 className=' translate-middle-y'>Record Vitals</h4>
+        <div className='vital_container d-flex justify-content-between '>
+          <div className='vitals'><GiMedicalPack size="25x"/></div>
+          <div className='vitals'><BiDonateBlood size="25x"/></div>
+          <div className='vitals'><TbReportAnalytics size="25x"/></div>
+          <div className='vitals'><RiVirusLine size="25x"/></div>
         </div>
+
     </div>
   )
 }
