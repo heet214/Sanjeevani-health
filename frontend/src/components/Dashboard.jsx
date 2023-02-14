208ff460397a568252dd9c37703e6d99bf487563
Aditya-k24 <80339924+Aditya-k24@users.noreply.github.com>
2023-02-14 23:31:45 +0530

    vitals

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Banners from './Banners'
 import HealthCard from './HealthCard'
+import RecordVitals from './RecordVitals'
 
 
 export default function Dashboard() {
@@ -8,6 +9,8 @@ export default function Dashboard() {
     <div>
       <div >
               <HealthCard/>
+              <RecordVitals/>
+              <Banners/>
 
       </div>
     </div>
