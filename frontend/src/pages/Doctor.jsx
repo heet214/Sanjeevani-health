8e8b8e79b1451f1f2ce9e629233151c492a7cfc9
Aditya-k24 <80339924+Aditya-k24@users.noreply.github.com>
2023-02-15 21:15:26 +0530

    doc

--- frontend/src/pages/Doctor.jsx
+++ frontend/src/pages/Doctor.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import '../css/doctor.css'
+import {icon} from '../assets/icon.png'
+import {icon1} from '../assets/icon1.png'
+import {icon2} from '../assets/icon2.png'
+import {icon3} from '../assets/icon3.png'
+import { Image } from 'react-bootstrap';
 
 function doctor() {
   return (
-    <div className="container">
+    <div className="container ">
     
-    <Card style={{ width: '22rem', padding: '0px' , margin:'auto' }}>
+    <Card style={{ width: '22rem', padding: '0px' , margin:'auto', marginTop:"7%",marginBottom:"9%" }}>
       <Card.Img className='doctorImage' variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-r_VImHWRaA2c-ck8KAn4cp-JT5yruwW6Ew&usqp=CAU" />
       <Card.Body>
         <Card.Title id="doctorName" style={{margin: '0px' , textAlign : 'left' ,fontSize:'18px' , fontFamily:'Poppins', fontWeight:'bold'}}>Dr. Yuvraj Thakur</Card.Title>
@@ -17,7 +22,22 @@ function doctor() {
       </Card.Body>
     </Card>
 
-    <div className='aboutMe'>
+    <div className="vital_container d-flex justify-content-around">
+        <div className="vitals med shadow-sm">
+          <Image src={icon} size="25x" />
+        </div>
+        <div className="vitals spo2 shadow-sm">
+          <Image src={icon1} size="25x" />
+        </div>
+        <div className="vitals report shadow-sm">
+          <Image src={icon2} size="25x" />
+        </div>
+        <div className="vitals virus shadow-sm">
+          <Image src={icon3} size="25x" />
+         </div>
+      </div>
+
+    <div className='aboutMe my-2' style={{marginLeft:"5%"}}>
       <p className='header'>About Me</p>
 
       <p className='subtext' id='aboutMeContent'>
@@ -26,14 +46,14 @@ function doctor() {
 
     </div>
 
-    <div className='patientInfo'>
+    <div className='patientInfo' style={{marginLeft:"5%"}}>
         <p className='header'>Patient Information</p>
         <div className='subtext' >Patient's Name: Ferrucio Tuccine </div>
         <div className='subtext' >Gender: Male </div>
 
     </div>
 
-    <button className='submitButton'>Book Appointment</button>
+    <button className='submitButton ' style={{color:"white"}}>Book Appointment</button>
 </div>
   )
 }
