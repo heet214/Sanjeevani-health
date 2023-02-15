2ae47e551dbf77c8ba024cd63c5dcc97a3f01515
unknown <yuvithakur7707@gmail.com>
2023-02-15 15:40:24 +0530

    login

--- frontend/src/components/HealthCard.jsx
+++ frontend/src/components/HealthCard.jsx
@@ -6,24 +6,6 @@ import profile2 from '../assets/profile.jpg';
 
 export default function HealthCard() {
   return (
-<<<<<<< HEAD
-    <div>
-      <div
-        className="card health-card rounded-5 position-absolute top-0 start-50 translate-middle-x my-5 shadow p-3 mb-5"
-        style={{ backgroundColor: '#1c6ba4' }}>
-        <div>
-          <Image
-            className="mx-2 rounded"
-            src={logo1}
-            style={{ height: '30px', width: '140px' }}
-          />
-
-          <Image
-            className="mx-auto float-right"
-            src={profile2}
-            style={{ height: '55px', width: '55px', borderRadius: '50%' }}
-          />
-=======
     <div >
 
         <div className="card health-card rounded-5 my-5 mx-auto shadow p-3 mb-5" style={{backgroundColor:"#1c6ba4", marginLeft:"-7rem"}}  >
@@ -34,7 +16,6 @@ export default function HealthCard() {
             <h5 className="card-title">Yuvraj Thakur</h5>
             <p className="card-text">Card Number....</p>
             
->>>>>>> b08e510d11d3ba3a9a6c4ab72c0040eb97b70aea
         </div>
         <div className="card-body my-2">
           <h5 className="card-title">Yuvraj Thakur</h5>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../css/signup.css'
+import logo from '../assets/colorLogo.png';
 
 const Signup = () => {
   const [name, setName] = useState('');
@@ -27,40 +29,53 @@ const Signup = () => {
 
   return (
     <div className="container">
-      <div className="register">
-        <label htmlFor="name">Enter your name</label>
+      <div>
+        <img className='logo' src={logo} />
+      </div>
+
+      <div className="register" >
+        <label className='label' htmlFor="name">Name</label> <br />
         <input
           type="text"
           name="name"
+          className='inputFeild'
           id="name"
+          placeholder='Enter Your Name'
           value={name}
           onChange={(e) => setName(e.target.value)}
-        />
-        <label htmlFor="email">Enter your email address</label>
+        /> 
+        <label htmlFor="email" className='label'>Email</label><br/>
         <input
           type="email"
+          className='inputFeild'
           id="email"
+          placeholder='Enter Your Email'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <label htmlFor="password">Password</label>
+        <label htmlFor="password" className='label'>Password</label>
         <input
           type="password"
           name="password"
           id="password"
+          placeholder='*******'
+          className='inputFeild'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <label htmlFor="birthdate">Enter your date of birth</label>
+        <label htmlFor="birthdate" className='label'>Date of birth</label>
         <input
           type="date"
           name="birthdate"
           id="birthdate"
+          className='inputFeild'
+          placeholder='dd/mm/yyyy'
           value={birthdate}
           onChange={(e) => setBirthdate(e.target.value)}
         />
       </div>
-      <button onClick={handleRegister}>Register</button>
+      <button className='button' onClick={handleRegister}>Register</button>
+      <p className='loginText'>Already Registered ? <strong>Login</strong> </p>
     </div>
   );
 };
