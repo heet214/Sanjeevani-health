643bd60e192f904b722333cd51b4efd0bdbce79a
deshna-gandhi <64076125+deshna-gandhi@users.noreply.github.com>
2023-02-16 03:34:51 +0530

    done adding bpm

---
+++ frontend/src/pages/Bpm.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+
+function Bpm() {
+    useEffect(() => {
+        window.location.replace('https://deshna-gandhi.github.io/')
+    }, [])
+  return (
+    <div>Bpm</div>
+  )
+}
+
+export default Bpm
