dbc3a444f76828f5fea08d520829c12bb6feaebd
Aditya-k24 <80339924+Aditya-k24@users.noreply.github.com>
2023-02-16 03:04:50 +0530

    vitals

--- frontend/src/components/HeartVitals.jsx
+++ frontend/src/components/HeartVitals.jsx
@@ -1,25 +1,60 @@
-import React from 'react'
-import {Bar} from 'react-chartjs-2'
+import React from "react";
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
 
-function HeartVitals() {
+const labels = ["January", "February", "March", "April", "May", "June"];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      data: [0, 10, 5, 2, 20, 30, 45],
+    },
+  ],
+};   
+
+const HeartVitals = () => {
   return (
     <div>
-        <Bar
-        data={{
-            labels:['Jan','Feb','Mar','Apr','May','June','Jul','Aug','Sept','Oct','Nov','Dec'],
-            datasets:[{
-                
-            }]
-        }}
-        height={80}
-        width={50}
-        options={{
-            maintainAspectRatio:false
-        }}
-        />
+      <Line data={data}/><br />
 
+        <div>
+           <h4> Previous Records</h4></div>
+      <div class="card">
+  <div class="card-header">
+    Date
+  </div>
+  <div class="card-body d-flex justify-content-between">
+  <div className="d-inline ">BPM</div>
+  <div className="d-inline">Spo2</div>
+  <div className="d-inline">Stress</div>
+  </div>
+</div>
+<div class="card">
+  <div class="card-header">
+    Date
+  </div>
+  <div class="card-body d-flex justify-content-between">
+  <div className="d-inline ">BPM</div>
+  <div className="d-inline">Spo2</div>
+  <div className="d-inline">Stress</div>
+  </div>
+</div>
+<div class="card">
+  <div class="card-header">
+    Date
+  </div>
+  <div class="card-body d-flex justify-content-between">
+  <div className="d-inline ">BPM</div>
+  <div className="d-inline">Spo2</div>
+  <div className="d-inline">Stress</div>
+  </div>
+</div>
     </div>
-  )
-}
+  );
+};
 
-export default HeartVitals  
+export default HeartVitals;
