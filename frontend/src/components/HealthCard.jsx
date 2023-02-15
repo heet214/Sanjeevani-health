83219f89474268e8a6e71e7a7b41d993e2f7443f
Heet <heetzatakia123@gmail.com>
2023-02-15 16:04:52 +0530

    tp

--- frontend/src/components/HealthCard.jsx
+++ frontend/src/components/HealthCard.jsx
@@ -6,26 +6,22 @@ import profile2 from '../assets/profile.jpg';
 
 export default function HealthCard() {
   return (
-    <div>
+    <div style={{ marginLeft: '6%' }}>
       <div
-        className="card health-card rounded-5 my-5 mx-auto shadow p-3 mb-5"
-        style={{ backgroundColor: '#1c6ba4', marginLeft: '-7rem' }}>
+        className="card health-card rounded-5 shadow mx-auto p-3 mb-5"
+        style={{ backgroundColor: '#1c6ba4', width: '95%', marginTop: '2rem' }}>
         <div>
           <Image
-            className="mx-2 rounded "
+            className="mx-2 rounded"
             src={logo1}
-            style={{ height: '35px', width: '140px', marginRight: '30%' }}
+            style={{ height: '30px', width: '140px' }}
           />
 
           <Image
-            className="mx-auto"
+            className="mx-auto float-right"
             src={profile2}
             style={{ height: '55px', width: '55px', borderRadius: '50%' }}
           />
-        </div>
-        <div className="card-body my-2">
-          <h5 className="card-title">Yuvraj Thakur</h5>
-          <p className="card-text">Card Number....</p>
         </div>
         <div className="card-body my-2">
           <h5 className="card-title">Yuvraj Thakur</h5>
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,53 +1,73 @@
-import React from 'react'
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
-import {FiLogOut} from 'react-icons/fi';
-import {BiArrowBack} from 'react-icons/bi'
+import React from 'react';
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBCard,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBBtn,
+  MDBTypography,
+  MDBIcon,
+} from 'mdb-react-ui-kit';
+import { FiLogOut } from 'react-icons/fi';
+import { CiLogin } from 'react-icons/ci';
 
 export default function Profile() {
   return (
     <div>
-     <div className="vh-100" style={{ backgroundColor: '#eee',marginTop:"-2rem" }}>
-      <MDBContainer className="container py-5 h-100">
-        <MDBRow className="justify-content-center align-items-center h-100">
-
-          <MDBCol md="12" xl="4">
-            <MDBCard style={{ borderRadius: '15px',height:"50rem" }}>
-            <div className="backBtn d-flex flex-start"  ><MDBBtn  style={{ borderRadius: '40%',}}>
-                    <BiArrowBack size={25}/>
-                </MDBBtn>
+      <div
+        className="vh-100"
+        style={{ backgroundColor: '#eee', marginTop: '-2rem' }}>
+        <MDBContainer className="container py-5 h-100">
+          <MDBRow className="justify-content-center align-items-center h-100">
+            <MDBCol md="12" xl="4">
+              <MDBCard style={{ borderRadius: '15px', height: '50rem' }}>
+                <div className="backBtn d-flex flex-start">
+                  <MDBBtn style={{ borderRadius: '40%' }}>
+                    <CiLogin />
+                  </MDBBtn>
                 </div>
 
-              <MDBCardBody className="text-center">
-                <div className="mt-3 mb-4">
-                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
-                    className="rounded-circle" fluid style={{ width: '100px' }} />
-                </div>
-                <MDBTypography tag="h4">Deshna Gandhi</MDBTypography>
-                <MDBCardText className="text-muted mb-4">
-                  @Customer <span className="mx-2">|</span> <a href="/">Sanjeevani.com</a>
-                </MDBCardText><br />
-                
-                <MDBBtn rounded size="lg" className='my-2 profile-btn'>
+                <MDBCardBody className="text-center">
+                  <div className="mt-3 mb-4">
+                    <MDBCardImage
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+                      className="rounded-circle"
+                      fluid
+                      style={{ width: '100px' }}
+                    />
+                  </div>
+                  <MDBTypography tag="h4">Deshna Gandhi</MDBTypography>
+                  <MDBCardText className="text-muted mb-4">
+                    @Customer <span className="mx-2">|</span>{' '}
+                    <a href="/">Sanjeevani.com</a>
+                  </MDBCardText>
+                  <br />
+
+                  <MDBBtn rounded size="lg" className="my-2 profile-btn">
                     Account Setting
-                </MDBBtn>
-                <MDBBtn rounded size="lg" className='my-2 profile-btn' >
-                  Privacy Policy
-                </MDBBtn>
-                <MDBBtn rounded size="lg" className='my-2 profile-btn'>
-                 Subscription
-                </MDBBtn>
-                <MDBBtn rounded size="lg" className='my-2 profile-btn'>
-                 Payment Setting
-                </MDBBtn>
-                <div className="text-center mt-5 mb-2">
-                  <FiLogOut size={30}/><br /> Log Out
-                </div>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+                  </MDBBtn>
+                  <MDBBtn rounded size="lg" className="my-2 profile-btn">
+                    Privacy Policy
+                  </MDBBtn>
+                  <MDBBtn rounded size="lg" className="my-2 profile-btn">
+                    Subscription
+                  </MDBBtn>
+                  <MDBBtn rounded size="lg" className="my-2 profile-btn">
+                    Payment Setting
+                  </MDBBtn>
+                  <div className="text-center mt-5 mb-2">
+                    <FiLogOut />
+                    <br /> Log Out
+                  </div>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
